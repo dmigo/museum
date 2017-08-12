b96b8e2cc8bd5f859023dfeb7d86383363762dc1
Dmitry Goryunov <dmitry.goryunov@zalando.de>
2017-08-12 14:25:26 +0200

    add keypad to ira

--- ira2/ira2.ino
+++ ira2/ira2.ino
@@ -1,4 +1,52 @@
 #include "rdm630.h"
+#include <Wire.h>
+#include <Keypad.h>
+
+#define SOLVED 1
+#define NOT_SOLVED 0
+
+class KeyCode{
+private:
+  static const byte _numRows = 4;
+  static const byte _numCols = 3;
+  char _keymap[_numRows][_numCols]= 
+  {
+  {'1', '2', '3'}, 
+  {'4', '5', '6'}, 
+  {'7', '8', '9'},
+  {'*', '0', '#'}
+  };
+  byte _rowPins[_numRows] = {12,11,10,9};
+  byte _colPins[_numCols]= {8,7,5};
+  Keypad _keypad= Keypad(makeKeymap(_keymap), _rowPins, _colPins, _numRows, _numCols);
+  
+  static const byte _numPassword = 5; // длинна пароля 
+  char _password[_numPassword] = {'3', '2', '1', '6', '7'}; // пароль 
+  
+  byte _currentIndex = 0;
+  bool _isOpen = false;
+public:
+  KeyCode(){
+  }
+  
+  void check(){
+    if(_isOpen)
+      return;
+    
+    char key = _keypad.getKey();
+    if(key != NO_KEY){
+      if(key == _password[_currentIndex])
+        _currentIndex++;
+      else
+        _currentIndex = 0;
+      
+      _isOpen = _currentIndex == _numPassword;
+    }
+  }
+  bool isOpen(){
+    return _isOpen;
+  }
+};
 
 class RfidLock{
 private:
@@ -48,6 +96,7 @@ public:
 };
 
 RfidLock *rfidLock;
+KeyCode* keyCode;
 
 
 void setup()
@@ -55,12 +104,20 @@ void setup()
   Serial.begin(9600);
   Serial.println("Starting...");
   rfidLock = new RfidLock(32157); //сюда кладем правильный ключ
+  keyCode = new KeyCode();
 }
 
 void loop()
 { 
   rfidLock->check();
-  if(rfidLock->isOpen())
-    Serial.println("OPEN");
+  keyCode->check();
+}
+
+void requestEvent() {
+  Serial.println("data requested");
+  if(keyCode->isOpen() && rfidLock->isOpen())
+    Wire.write(SOLVED); 
+  else
+    Wire.write(NOT_SOLVED);
 }
 
