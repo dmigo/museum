579699325ce32e5a979a7c0a6e53153b965a223f
Dmitry Goryunov <d.f.goryunov@gmail.com>
2017-08-29 10:30:16 +0200

    momaDog talk to the masterMind

--- momaDog/momaDog.ino
+++ momaDog/momaDog.ino
@@ -3,32 +3,87 @@
 #define SOLVED 1
 #define NOT_SOLVED 0
 
-const int amount = 3;
+const int amount = 4;
 int slaves[amount] = { 8, 9, 10, 11};
-int leds[amount] = { 8, 9, 10, 11}; 
+int leds[amount] = { 8, 9, 10, 11};
+bool state[amount] = {false};
 
 void setup() {
   Wire.begin(); // join i2c bus (address optional for master)
-  Serial.begin(9600);
+  Serial1.begin(115200);
+  Serial2.begin(9600);
   for(int i=0; i<amount; i++){
     pinMode(leds[i], OUTPUT);
     digitalWrite(leds[i], LOW);
   }
 }
 
-void loop() {
-  for(int i = 0; i < amount; i++)
-  {
-    Wire.requestFrom(slaves[i], 1);    // request 1 byte from slave device
-    if (Wire.available()) { 
-      byte b = Wire.read(); 
-      Serial.print(b); 
-      Serial.print(" from "); 
-      Serial.println(slaves[i]);
-      if(b == SOLVED)
+void setState(int id, bool newState) {
+  state[id] = state[id] || newState;
+  sendState();
+  render();
+}
+
+void sendState() {
+  for (int i = 0; i < amount; i++) {
+    Serial1.print('C');
+    Serial1.print(i);
+    Serial1.print(state[i]);
+  }
+}
+
+void skipToCommand() {
+  if (Serial1.available() <= 0)
+    return;
+    
+  while(Serial1.peek() != 'C'){
+    Serial1.read();
+  }
+}
+
+void executeCommand() {
+  if (Serial1.available() <= 0
+  || Serial1.peek() != 'C')
+    return;
+
+  int id = Serial1.parseInt();
+  bool newState = Serial1.parseInt();
+
+  state[id] = state[id] || newState;
+}
+
+void readState() {
+  skipToCommand();
+  executeCommand();
+  sendState();
+  render();
+}
+
+void render() {
+  for (int i = 0; i < amount; i++) {
+    if(state[i])
         digitalWrite(leds[i], HIGH);
       else
         digitalWrite(leds[i], LOW);
+  }
+}
+
+void querySlaves(){
+  for(int id = 0; id < amount; id++)
+  {
+    Wire.requestFrom(slaves[id], 1);    // request 1 byte from slave device
+    if (Wire.available()) { 
+      byte b = Wire.read(); 
+      Serial2.print(b); 
+      Serial2.print(" from "); 
+      Serial2.println(slaves[id]);
+
+      setState(id, b == SOLVED);
     }
   }
+}
+
+void loop() {
+  querySlaves();
+  readState();
 }
