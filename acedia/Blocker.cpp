3a06dd9734d2351d73f629161bdd7bfe9d020d08
dmigo <d.f.goryunov@gmail.com>
2017-11-30 20:34:22 +0100

    More advanced Blocker

--- acedia/Blocker.cpp
+++ acedia/Blocker.cpp
@@ -3,32 +3,39 @@
 class Blocker {
   private:
     bool _blocked;
+    
+    bool _blockedOnTime;
     long _whenBlocked;
     long _duration;
 
   public:
     Blocker() {
       _blocked = false;
+      _blockedOnTime = false;
     }
+    void check() {
+      if (!_blockedOnTime)
+        return;
+        
+      if (millis() > _whenBlocked + _duration)
+        _blockedOnTime = false;
+    }
+    
     void block(long milliseconds) {
       _whenBlocked = millis();
       _duration = milliseconds;
-      _blocked = true;
+      _blockedOnTime = true;
     }
-    void check() {
-      if (!_blocked)
-        return;
-      if (millis() > _whenBlocked + _duration)
-        _blocked = false;
-    }
+    
     void block() {
       _blocked = true;
     }
     void unblock() {
       _blocked = false;
+      _blockedOnTime = false;
     }
     bool isBlocked() {
-      return _blocked;
+      return _blocked || _blockedOnTime;
     }
 };
 
