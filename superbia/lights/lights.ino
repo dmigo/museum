def161daacda8617f35894747afa19b05dc55a30
dmigo <d.f.goryunov@gmail.com>
2017-12-14 23:26:50 +0100

    superbia lights move Waiter to a separate file

--- superbia/lights/lights.ino
+++ superbia/lights/lights.ino
@@ -1,28 +1,5 @@
-class Waiter {
-  private:
-    bool _isWaiting = false;
-    long _since = 0;
-    long _limit;
-  public:
-    Waiter(long limit) {
-      _limit = limit;
-    }
-
-    void start() {
-      _since = millis();  
-      _isWaiting = true;
-    }
-    void drop() {
-      _isWaiting = false;
-    }
-
-    bool isFinished() {
-      return _isWaiting &&
-        _since + _limit < millis();
-    }
-};
-
 #include <Wire.h>
+#include "Waiter.cpp"
 
 #define DELAY 3000 // задержка перед включением
 //#define ADDRESS 
