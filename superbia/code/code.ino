4a3b2014d11644464a48918d175e860ee94857b2
dmigo <d.f.goryunov@gmail.com>
2017-12-19 18:42:08 +0100

    Indication for superbia

--- superbia/code/code.ino
+++ superbia/code/code.ino
@@ -2,11 +2,13 @@
 
 #include <Wire.h>
 #include "KeyCode.cpp"
+#include "GameStateIndication.cpp"
 
 #define SOLVED 1
 #define NOT_SOLVED 0
 
 KeyCode* rightCode;
+GameStateIndication* indication;
 
 static const byte sizeRightPassword = 5; // длинна правильного пароля 
 char rightPassword[sizeRightPassword] = {'р', 'а', 'й', 'с', 'к', 'и', 'й'}; // правильный пароль
@@ -16,7 +18,10 @@ void setup()
   Serial.begin(9600);
   Serial.println("Starting...");
   
+  indication = new GameStateIndication(A1, A0);// зеленый индикатор, красный индикатор
   rightCode = new KeyCode(sizeRightPassword, rightPassword);
+  rightCode->onSuccess(codeSolved);
+  rightCode->onFailure(codeFailed);
   
   Wire.begin(ADDRESS);
   Wire.onRequest(requestEvent);
@@ -26,10 +31,22 @@ void setup()
 void loop()
 {
   rightCode->check();
+  indication->check();
 }
+
 void requestEvent() {
   if(rightCode->isSolved())
     Wire.write(SOLVED); 
   else
     Wire.write(NOT_SOLVED);
 }
+
+void codeSolved(){
+ Serial.println("Win");
+ indication->win();
+}
+void codeFailed(){
+ Serial.println("Fail");
+ indication->fail();
+}
+
