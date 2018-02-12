39be510e8ff6dcd8b9d36751b3319c3dc6242001
k0ri0n <burkovwp@gmail.com>
2018-02-13 01:21:44 +0700

    codes

---
+++ Code_panel_1/Code_panel_1.ino
@@ -0,0 +1,52 @@
+#define ADDRESS 23
+
+#include <Wire.h>
+#include "KeyCode.cpp"
+#include "GameStateIndication.cpp"
+
+#define SOLVED 1
+#define NOT_SOLVED 0
+
+KeyCode* rightCode;
+GameStateIndication* indication;
+
+static const byte sizeRightPassword = 4; // длинна правильного пароля 
+char rightPassword[sizeRightPassword] = {'3', '8', '2', '9'}; // правильный пароль
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Starting...");
+  
+  indication = new GameStateIndication(10, 11);// зеленый индикатор, красный индикатор
+  rightCode = new KeyCode(sizeRightPassword, rightPassword);
+  rightCode->onSuccess(codeSolved);
+  rightCode->onFailure(codeFailed);
+  
+  Wire.begin(ADDRESS);
+  Wire.onRequest(requestEvent);
+  Serial.println("Started.");
+}
+
+void loop()
+{
+  rightCode->check();
+  indication->check();
+}
+
+void requestEvent() {
+  if(rightCode->isSolved())
+    Wire.write(SOLVED); 
+  else
+    Wire.write(NOT_SOLVED);
+}
+
+void codeSolved(){
+ Serial.println("Win");
+ indication->win();
+}
+void codeFailed(){
+ Serial.println("Fail");
+ indication->fail();
+}
+
